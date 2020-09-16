5344c17afc707ae4215134d777be92d0a90c0fa9
Anatoly <itrm000@gmail.com>
2020-09-16 20:03:59 +0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        long ticket = 2657;
-        long bonus = 20;
-        long mile = 1;
-        long sumMile = ticket/bonus * mile;
+        long ticket = 2657; /* цена билета */
+        long bonus = 20;    /* бонус - за какой количество рублей начисляется 1 миля*/
+        long sumMile = ticket/bonus;  /* суммарный бонус за билет */
 
         System.out.println(sumMile);
     }
