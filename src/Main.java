b8c3817374900537924a02aa9e64e81ca60f770e
Anatoly <itrm000@gmail.com>
2020-09-16 19:56:29 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long ticket = 2657;
+        long bonus = 20;
+        long mile = 1;
+        long sumMile = ticket/bonus * mile;
+
+        System.out.println(sumMile);
+    }
+}
