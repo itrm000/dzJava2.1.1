0e4e1a97df8caa55a9e8745e3ba84f381c053586
Anatoly <itrm000@gmail.com>
2020-12-02 20:53:46 +0500

    new price commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        long ticket = 2657; /* цена билета */
+        long ticket = 1250; /* цена билета */
         long bonus = 20;    /* бонус - за какой количество рублей начисляется 1 миля*/
         long sumMile = ticket/bonus;  /* суммарный бонус за билет */
 
